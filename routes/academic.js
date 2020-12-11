b739c6f864eaaa8195714def667935370e561183
IvanZzz <ivan.nzhong@gmail.com>
2020-12-11 13:43:39 -0600

    add loading team comment table

--- routes/academic.js
+++ routes/academic.js
@@ -183,6 +183,38 @@ router.get('/team_grades/fetch-fifty-rows/:start_id', (req, res) => {
   //res.end()
 });
 
+//get the next 50 rows from database with id greater than start_id
+router.get('/team_comments/fetch-fifty-rows/:start_id', (req, res) => {
+  console.log("trying to fetch rows based on entered index and attribute")
+
+  const connection = getConnection();
+
+  const start_id = req.params.start_id;
+  const end_id = Number(start_id) + 28;
+
+  //exclude the start, include the end
+  let queryString = "SELECT * FROM team_comments WHERE id >= " + start_id + " AND id < " + end_id
+  console.log("the query string to fetch 50 rows is: "  + queryString)
+  connection.query(queryString, (err, rows, fields) => {
+    if (err) {
+      console.log("Failed to fetch 50 more rows: " + err);
+      res.sendStatus(500);
+      return;
+    }
+
+    console.log("I think we fetched 50 more rows seccessfully")
+    
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    
+    res.json(rows)
+  })
+  setTimeout(() => {
+    connection.end();
+  }, 5 * 1000);
+  //res.end()
+});
+
 
 // updates the tables under the academic simulation
 router.post('/update', (req, res) => {
